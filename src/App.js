24b171b3b85af596d8f5dede66677baeb637c06b
Darlington02 <darlingtonnnam@gmail.com>
2024-10-25 05:36:19 +0100

    chore: add test impl

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
+// This app was tested locally. clone the irys bundle impl, and run `npm link @irys/bundles` to make package avaialable
 import logo from './logo.svg';
 import './App.css';
+import { RpcProvider, WalletAccount } from "starknet";
+import { connect } from "get-starknet";
+import { InjectedStarknetSigner } from "@irys/bundles"
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
 
 function App() {
+  const provider = new RpcProvider({
+    nodeUrl: "https://starknet-sepolia.public.blastapi.io/rpc/v0_7",
+  });
+
+  const signMessage = async() => {
+    const wallet = await connect({ modalMode: 'alwaysAsk', modalTheme: 'light'});
+
+    const walletAccount = new WalletAccount({ nodeUrl: provider }, wallet);
+    const signer = new InjectedStarknetSigner(provider, walletAccount);
+    await signer.init();
+    
+    let signature = await signer.sign(Buffer.from("Hello Irys!"));
+    console.log(Buffer.from(signature))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="button cursor-pointer px-5 pt-5 py-3 text-white bg-[#0C0C4F]" onClick={signMessage}>
+          Connect & Sign
+        </button>
       </header>
     </div>
   );
