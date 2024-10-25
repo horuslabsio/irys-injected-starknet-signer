a53b84d248c8bc2e1aaa2af5fdb2ec6ecea32f96
Darlington02 <darlingtonnnam@gmail.com>
2024-10-26 00:38:44 +0100

    feat: veifyMessage, use starknetkit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { RpcProvider, WalletAccount } from "starknet";
-import { connect } from "get-starknet";
+import { connect } from "starknetkit";
 import { InjectedStarknetSigner } from "@irys/bundles"
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
@@ -12,15 +12,27 @@ function App() {
     nodeUrl: "https://starknet-sepolia.public.blastapi.io/rpc/v0_7",
   });
 
+  let signature;
   const signMessage = async() => {
-    const wallet = await connect({ modalMode: 'alwaysAsk', modalTheme: 'light'});
+    const { wallet } = await connect({ modalMode: "alwaysAsk"});
 
     const walletAccount = new WalletAccount({ nodeUrl: provider }, wallet);
+
     const signer = new InjectedStarknetSigner(provider, walletAccount);
     await signer.init();
     
-    let signature = await signer.sign(Buffer.from("Hello Irys!"));
+    const message = "Hello Irys!";
+    signature = await signer.sign(Buffer.from(message));
     console.log(Buffer.from(signature))
+  }
+
+  const verifyMessage = async() => {
+    const pubKey = "0x030572512eee445da3b22322ff07ebce73e2e46237c73d79a9a841620c3a092d79"; // replace with your public key
+    const message = "Hello Irys!";
+
+    const hexString = pubKey.startsWith("0x") ? pubKey.slice(2) : pubKey;
+    const isValid = InjectedStarknetSigner.verify(Buffer.from(hexString, "hex"), Buffer.from(message), signature);
+    console.log(isValid);
   }
 
   return (
@@ -29,6 +41,10 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <button className="button cursor-pointer px-5 pt-5 py-3 text-white bg-[#0C0C4F]" onClick={signMessage}>
           Connect & Sign
+        </button>
+
+        <button className="button cursor-pointer px-5 pt-5 py-3 text-white bg-[#0C0C4F]" onClick={verifyMessage}>
+          Verify message
         </button>
       </header>
     </div>
